08a9d309b863cc0508d14266714be6b85281f2c7
ss251 <ss251@uw.edu>
2021-04-14 16:13:03 -0700

    add random plays

--- app/src/main/java/edu/uw/ss251/dotify/MainActivity.kt
+++ app/src/main/java/edu/uw/ss251/dotify/MainActivity.kt
@@ -2,10 +2,20 @@ package edu.uw.ss251.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlin.random.Random
+
+private val noOfPlays = Random.nextInt(1000, 1000000)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val noPlays = findViewById<TextView>(R.id.noPlays)
+
+
+        noPlays.text = noOfPlays.toString() + " plays"
+
     }
 }
