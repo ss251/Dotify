70d911a59299d679a15446746c78892413d83b3e
ss251 <ss251@uw.edu>
2021-04-14 17:57:17 -0700

    add functionality

--- app/src/main/java/edu/uw/ss251/dotify/MainActivity.kt
+++ app/src/main/java/edu/uw/ss251/dotify/MainActivity.kt
@@ -2,10 +2,16 @@ package edu.uw.ss251.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.ViewSwitcher
+import androidx.core.view.isVisible
 import kotlin.random.Random
 
 private val noOfPlays = Random.nextInt(1000, 1000000)
+private var plays = noOfPlays
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +21,38 @@ class MainActivity : AppCompatActivity() {
         val noPlays = findViewById<TextView>(R.id.noPlays)
 
 
-        noPlays.text = noOfPlays.toString() + " plays"
+        noPlays.text = noOfPlays.toString()
 
+    }
+
+    fun clickPrevious(view: View) {
+        Toast.makeText(this, "Skipping to previous track", Toast.LENGTH_SHORT).show()
+    }
+
+    fun clickPlay (view: View) {
+        val noPlays = findViewById<TextView>(R.id.noPlays)
+        plays += 1
+        noPlays.text = (plays).toString()
+    }
+
+    fun clickNext (view: View) {
+        Toast.makeText(this, "Skipping to next track", Toast.LENGTH_SHORT).show()
+    }
+
+    fun clickChangeUser(view: View) {
+        val userName = findViewById<TextView>(R.id.userName)
+        val userNameEdit = findViewById<TextView>(R.id.userNameEdit)
+        val changeUser = findViewById<TextView>(R.id.changeUser)
+        val switcher = findViewById<ViewSwitcher>(R.id.viewSwitcher)
+        if(changeUser.text == "Apply") {
+            userNameEdit.isVisible = false
+            userName.text = userNameEdit.text
+            userName.isVisible = true
+            changeUser.text = "Change User"
+            return
+        }
+        userName.isVisible = false
+        switcher.showNext()
+        changeUser.text = "Apply"
     }
 }
