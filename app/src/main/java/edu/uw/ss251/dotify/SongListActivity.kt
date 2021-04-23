678c5b434454220594b3dc4607ee24c87393d923
ss251 <ss251@uw.edu>
2021-04-23 01:39:37 -0700

    add header to defaultview

--- app/src/main/java/edu/uw/ss251/dotify/SongListActivity.kt
+++ app/src/main/java/edu/uw/ss251/dotify/SongListActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 
 class SongListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_song_list)
+        title = getString(R.string.default_header)
     }
 }
