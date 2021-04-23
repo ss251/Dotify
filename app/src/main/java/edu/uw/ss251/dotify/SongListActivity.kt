0926226ca4881d539ff3745fc53ab9d9ad46f0d0
ss251 <ss251@uw.edu>
2021-04-23 01:16:41 -0700

    make SongListActivity.kt as default view

---
+++ app/src/main/java/edu/uw/ss251/dotify/SongListActivity.kt
@@ -0,0 +1,11 @@
+package edu.uw.ss251.dotify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SongListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_list)
+    }
+}
